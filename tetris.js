f431231b21b0cf5434d36d476fc365cef2292a23
Terkadur <47321551+Terkadur@users.noreply.github.com>
2020-10-14 14:12:53 +0300

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -1,25 +1,3 @@
-/*NOTE TO FUTURE SELF
-CONTINUOUSLY CHECK FOR POSSIBLE LOCATIONS WHILE EXCLUDING UNREACHABLE ONES
-Add in holding pieces and upcoming pieces
-Variables:
-height
-holes
-blocks above holes
-no holes if line is cleared
-ledges
-good for next piece
-bumpiness
-blocks in rightmost lane (or penalize empty pillars?)
-clearing less than 4 lines
-*/
-
-
-/*Holding rules
-Can switch any time
-Switch causes current piece to be held and held piece starts at the top
-Cannot switch back
-If no held, go to next piece
-*/
 board_width = 10;
 board_height = 20;
 scl = 16;
@@ -49,7 +27,9 @@ hold_lock = false;
 var next_x, next_y, held_x, held_y;
 var real_path = [];
 
+//setup is called once when the code is first executed
 function setup() {
+  //sets up the board
   for (let i = 0; i < board_width; i++) {
     grid[i] = [];
     for (let j = 0; j < board_height + 2; j++) {
@@ -70,28 +50,30 @@ function setup() {
   createCanvas(board_width*scl + scl*x_gap, board_height*scl + scl*y_gap);
   frameRate(_fr_);
 
+  //position for "next" piece
   next_x = scl + width - scl*x_gap/2 + (scl*x_gap/2 - scl*4)/2;
   next_y = scl*y_gap/2;
   next_x = (next_x - scl*x_gap/2)/scl;
   next_y = (height - next_y - scl*y_gap/2)/scl;
   next_y -= 2;
 
-
-  //scl*i + (scl*x_gap/2 - scl*4)/2
+  //position for "held" piece
   held_x = scl + (scl*x_gap/2 - scl*4)/2;
   held_y = scl*y_gap/2;
   held_x = (held_x - scl*x_gap/2)/scl;
   held_y = (height - held_y - scl*y_gap/2)/scl;
   held_y -= 2;
   
+  //creates new piece and next piece
   piece = new Tetromino(4, board_height - 2, random(["I", "O", "T", "S", "Z", "J", "L"]));
   next_piece = random(["I", "O", "T", "S", "Z", "J", "L"]);
   next_show = new Tetromino(next_x, next_y, next_piece);
 }
 
-
+//draw is called continuously after setup
 function draw() {
   for (let c = 0; c < _cpt_; c++) {
+    //clear full rows
     let rows_cleared = [];
     for (let j = 0; j < board_height; j++) {
       let full_row = true;
@@ -107,6 +89,8 @@ function draw() {
       }
     }
     clearRow(rows_cleared);
+    
+    //scoring
     if (rows_cleared.length >= 4) {
       score += 400;
       if (tetris_mode) {
@@ -116,10 +100,8 @@ function draw() {
     } else if (rows_cleared.length > 0) {
       tetris_mode = false;
     }
-    if (rows_cleared.length > 0) {
-      // print(score);
-    }
     
+    //ai scans the board
     if (ai_active) {
       //newAI
       artIntel(piece.x, piece.y, piece.letter, piece.rot, grid);
@@ -127,35 +109,41 @@ function draw() {
       //oldAI
       // artIntel(piece.x, piece.y, piece.shape, piece.rot, grid);
     }
-
+    
     if (t % (wait_time*_fr_) == wait_time*_fr_ - 1) {
+      //move down piece automatically, if can't, make a new piece
       if (!piece.moveD()) {
         newPiece();
       }
     } else {
       if (!ai_active) {
+        //player controlled down movement
         if (keyIsDown(DOWN_ARROW)) {
           piece.moveD();
         }
       } else {
+        //ai controlled down movement
         if (target_down) {
           piece.moveD();
         }
       }
     }
     
+    //ai controlled left/right movement
     if (target_left) {
       piece.moveL();
     } else if (target_right) {
       piece.moveR();
     }
 
+    //ai controlled left/right rotation
     if (target_rot_l) {
       piece.turnL();
     } else if (target_rot_r) {
       piece.turnR();
     }
     
+    //detect when new piece collides with an old piece, end the game
     let lose = false;
     for (let j = 0; j < piece.shape.length; j++) {
       if (String(grid[piece.x + piece.shape[j][0]][piece.y + piece.shape[j][1]]) != String(color(base_color))) {
@@ -172,6 +160,7 @@ function draw() {
 
   background(96);
 
+  //draw the board
   for (let i = 0; i < board_width; i++) {
     for (let j = 0; j < board_height; j++) {
       fill(grid[i][j]);
@@ -179,9 +168,10 @@ function draw() {
     }
   }
 
+  //draw the piece
   piece.show();
 
-  //hold area
+  //draw hold area
   fill(base_color);
   textSize(scl);
   textAlign(CENTER, BOTTOM)
@@ -195,7 +185,7 @@ function draw() {
     held_show.show();
   }
 
-  //next area
+  //draw next area
   fill(base_color);
   textSize(scl);
   textAlign(CENTER, BOTTOM)
@@ -207,13 +197,14 @@ function draw() {
   }
   next_show.show();
 
-  //score
+  //draw score
   fill(base_color);
   textSize(scl);
   textAlign(CENTER, BOTTOM)
   text("SCORE: " + score, width/2, scl*y_gap/2);
 }
 
+//class to make pieces
 function Tetromino(x, y, letter, rot = 0) {
   this.x = x;
   this.y = y;
@@ -221,10 +212,12 @@ function Tetromino(x, y, letter, rot = 0) {
   this.col = letter_dict[this.letter][0];
   this.shape = [];
   this.rot = rot;
+  //get the shape of the piece
   for (let i = 0; i < letter_dict[this.letter][1].length; i++) {
     this.shape[i] = [...letter_dict[this.letter][1][i]];
   }
   
+  //rotate the piece to its set rotation
   for (let j = 0; j < this.rot; j++) {
     for (let i = 0; i < this.shape.length; i++) {
       let r = dist(this.shape[i][0], this.shape[i][1], 0, 0);
@@ -235,12 +228,15 @@ function Tetromino(x, y, letter, rot = 0) {
     }
   }
 
+  //draw the piece
   this.show = function() {
     fill(this.col);
     for (let j = 0; j < this.shape.length; j++) {
       rect(scl*(this.x + this.shape[j][0]) + scl*x_gap/2, height - scl*(this.y + this.shape[j][1] + 1) - scl*y_gap/2, scl, scl);
     }
   };
+  
+  //piece movements
   this.moveD = function() {
     let move_d = true;
     for (let j = 0; j < this.shape.length; j++) {  
@@ -252,6 +248,7 @@ function Tetromino(x, y, letter, rot = 0) {
     }
     if (move_d) {
       this.y -= 1;
+      t = 0;
     }
     return move_d;
   };
@@ -371,6 +368,7 @@ function Tetromino(x, y, letter, rot = 0) {
   };
 }
 
+//player controlled left/right movement
 function keyPressed() {
   if (!ai_active) {
     if (keyCode === LEFT_ARROW) {
@@ -381,6 +379,7 @@ function keyPressed() {
   }
 }
 
+//player controlled left/right turning and hold piece
 function keyTyped() {
   if (!ai_active) {
     if (key === "a") {
@@ -414,11 +413,14 @@ function holdPiece() {
 
 function newPiece() {
   hold_lock = false;
+  //"solidify" piece into board
   for (let j = 0; j < piece.shape.length; j++) {
     let old_x = piece.x + piece.shape[j][0];
     let old_y = piece.y + piece.shape[j][1];
     grid[old_x][old_y] = piece.col;
   }
+  
+  //get next piece
   piece = new Tetromino(4, 18, next_piece);
   next_piece = random(["I", "O", "T", "S", "Z", "J", "L"]);
   next_show = new Tetromino(next_x, next_y, next_piece);
@@ -444,6 +446,7 @@ function youLose() {
 function artIntel(x, y, letter, rot, board) {
   let cost_min = 1000;
   if (t == 1) {
+    //scans possible locations
     for (let x_pos = 0; x_pos < board.length; x_pos++) {
       for (let y_pos = 0; y_pos <= y; y_pos++) {
         for (let rot_pos = 0; rot_pos < 4; rot_pos++) {
@@ -479,58 +482,14 @@ function artIntel(x, y, letter, rot, board) {
           if (!viable || !resting) {
             continue;
           }
-
-          //viable move?
+          
           /*
-          keep moves in a backwards list
-          move up every wait_time*_fr_ frames (needed?)
-
-          if real right of test: try move right
-          if doesn't work, try left/rotating
-
-          if real up of test: try move up
-          if real left of test: try move left
-
+          TODO
+          check for paths to candidate location (likely using a test Tetromino)
+          store path as a sequence of moves
           */
           
-          // let p = 0;
-          // while (x_pos != x || y_pos != y || rot_pos != rot) {
-          //   //aligns horizontally if possible
-          //   if (x_pos < x) {
-          //     if (test_piece.moveR()) {
-          //       test_path.unshift("ml");
-          //     }  
-          //   } else if (x_pos > x) {
-          //     if (test_piece.moveL()) {
-          //       test_path.unshift("mr");
-          //     }
-          //   } else if (y_pos < y) {
-          //     if (test_piece.moveU()) {
-          //       test_path.unshift("md");
-          //     }
-          //   } else if (rot_pos != rot) {
-          //     let dif = rot - rot_pos;
-          //     if (abs(dif) == 3) {
-          //       dif *= -1/3;
-          //     }
-          //     if (dif > 0) {
-          //       if (test_piece.turnR()) {
-          //         test_path.unshift("tl");
-          //       }
-          //     } else {
-          //       if (test_piece.turnL()) {
-          //         test_path.unshift("tr");
-          //       }
-          //     }
-          //   }
-          //   p++;
-          //   if (p > 100) {
-          //     print(x_pos, y_pos);
-          //     break;
-          //   }
-          // }
-          
-
+          //scores possible locations
           let y_max = 0;
           let empty_test = 0;
           for (let j = 0; j < test_piece.shape.length; j++) {
@@ -591,210 +550,23 @@ function artIntel(x, y, letter, rot, board) {
           
           let cost_test = height_coef*y_max + hole_coef*empty_test + clear_coef*clear_test;
           if (cost_test < cost_min) {
-            // let test_path = [];
-            // while (test_piece.x != x) {
-            //   if (test_piece.x < x) {
-            //     if (test_piece.moveR()) {
-            //       test_path.unshift("ml");
-            //     } else if (test_piece.moveU()) {
-            //       test_path.unshift("md");
-            //     }
-            //   } else if (test_piece.x > x) {
-            //     if (test_piece.moveL()) {
-            //       test_path.unshift("mr");
-            //     } else if (test_piece.moveU()) {
-            //       test_path.unshift("md");
-            //     }
-            //   }
-            // }
-            // print("x");
-            // while (test_piece.y != y) {
-            //   if (test_piece.moveU()) {
-            //     test_path.unshift("md");
-            //   }
-            //   print(test_piece.y, y);
-            // }
-            // print("y");
-            // while (test_piece.rot != rot) {
-            //   let dif = rot - test_piece.rot;
-            //   if (abs(dif) == 3) {
-            //     dif *= -1/3;
-            //   }
-            //   if (dif > 0) {
-            //     if (test_piece.turnR()) {
-            //       test_path.unshift("tl");
-            //     }
-            //   } else {
-            //     if (test_piece.turnL()) {
-            //       test_path.unshift("tr");
-            //     }
-            //   }
-            // }
-            // print("rot");
-            // //CHEATING
+            //CHEATING teleports piece to best location
             piece.x = test_piece.x;
             piece.y = test_piece.y;
             piece.shape = test_piece.shape;
             cost_min = cost_test;
-            // real_path = test_path;
+            /*
+            TODO
+            save best path
+            delete cheating teleport
+            */
           }
         }
       }
     }
   }
-  // if (real_path.length > 0) {
-  //   target_right = false;
-  //   target_left = false;
-  //   target_down = false;
-  //   target_rot_r = false;
-  //   target_rot_l = false;
-  //   if (real_path[0] == "mr") {
-  //     target_right = true;
-  //   } else if (real_path[0] == "ml") {
-  //     target_left = true;
-  //   } else if (real_path[0] == "md") {
-  //     target_down = true;
-  //   } else if (real_path[0] == "tl") {
-  //     target_rot_l = true;
-  //   } else if (real_path[0] == "tr") {
-  //     target_rot_r = true;
-  //   }
-  //   real_path.shift();
-  // }
+  /*
+  TODO
+  go through path moves using target_left, target_right, target_down, target_rot_l, target_rot_r
+  */
 }
-
-//oldAI
-// function artIntel(x, y, shape, rot, board) {
-//   let cost_min = 1000;
-//   if (t == 1) {
-//     for (let n = 0; n < 4; n++) {
-//       let x_min = 0;
-//       let x_max = 0;
-//       for (let j = 0; j < shape.length; j++) {
-//         if (shape[j][0] < x_min) {
-//           x_min = shape[j][0];
-//         } else if (shape[j][0] > x_max) {
-//           x_max = shape[j][0];
-//         }
-//       }
-      
-//       for (let i = 0; i < board_width; i++) {
-//         if (i + x_min < 0) {
-//           continue;
-//         } else if (i + x_max >= board_width) {
-//           continue;
-//         }
-        
-//         let move_d = true;
-//         let y_test = y;
-//         while (move_d) {
-//           for (let j = 0; j < shape.length; j++) {  
-//             if (shape[j][1] + y_test == 0) {
-//               move_d = false;
-//             } else if (String(board[shape[j][0] + i][shape[j][1] + y_test - 1]) != String(color(base_color))) {
-//               move_d = false;
-//             }
-//           }
-//           if (move_d) {
-//             y_test -= 1;
-//           }
-//         }
-        
-//         let y_max = 0;
-//         let empty_test = 0;
-//         for (let j = 0; j < shape.length; j++) {
-//           //height
-//           if (y_test + shape[j][1] > y_max) {
-//             y_max = y_test + shape[j][1];
-//           }
-          
-//           //holes
-//           let check_below = true;
-//           let check_y = 1;
-//           while (check_below) {
-//             check_below = false;
-//             if (shape[j][1] + y_test - check_y >= 0) {
-//               check_below = true;
-//               if (board[shape[j][0] + i][shape[j][1] + y_test - check_y] == String(color(base_color))) {
-//                 let over_cell = false;
-//                 for (let k = 0; k < shape.length; k++) {
-//                   if (k == j) {
-//                     continue;
-//                   } else if (shape[j][0] == shape[k][0] && shape[j][1] - 1 == shape[k][1]) {
-//                     over_cell = true;
-//                     check_below = false;
-//                     break;
-//                   }
-//                 }
-//                 if (!over_cell) {
-//                   empty_test++;
-//                   check_below = true;
-//                 }
-//               }
-//               check_y += 1;
-//             }
-//           }
-//         }
-        
-//         //clear
-//         let clear_test = 0;
-//         for (let j = 0; j < board_height; j++) {
-//           let full_row = true;
-//           for (let k = 0; k < board_width; k++) {
-//             if (String(grid[k][j]) == String(color(base_color))) {
-//               full_row = false;
-//               for (let m = 0; m < shape.length; m++) {
-//                 if (k == i + shape[m][0] && j == y_test + shape[m][1]) {
-//                   full_row = true;
-//                 }
-//               }
-//               if (!full_row) {
-//                 break;
-//               }
-//             }
-//           }
-//           if (full_row) {
-//             clear_test++;
-//           }
-//         }
-        
-//         let cost_test = height_coef*y_max + hole_coef*empty_test + clear_coef*clear_test;
-//         if (cost_test < cost_min) {
-//           x_target = i;
-//           rot_target = n;
-//           cost_min = cost_test;
-//         }
-//       }
-      
-//       for (let j = 0; j < shape.length; j++) {
-//         let r = dist(shape[j][0], shape[j][1], 0, 0);
-//         let theta = atan2(shape[j][1], shape[j][0]);
-//         theta -= PI/2;
-//         shape[j][0] = round(r*cos(theta));
-//         shape[j][1] = round(r*sin(theta));
-//       }
-//     }
-//   }
-
-  
-//   target_left = false;
-//   target_right = false;
-//   if (x_target < x) {
-//     target_left = true;
-//   } else if (x < x_target) {
-//     target_right = true;
-//   }
-  
-//   target_rot_l = false;
-//   target_rot_r = false;
-//   if (rot_target - rot == 3) {
-//     target_rot_l = true;
-//   } else if (rot_target - rot > 0) {
-//     target_rot_r = true;
-//   }
-  
-//   target_down = false;
-//   if (!target_left && !target_right && !target_rot_r && !target_rot_l) {
-//     target_down = true;
-//   }
-// }
